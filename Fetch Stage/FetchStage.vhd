3dbd8d42622571c375ebe5d622084798d3edd6a9
nada-adel-mohamady <39887982+nada-adel-mohamady@users.noreply.github.com>
2020-05-05 22:06:42 +0200

    fetch output component of IFID

--- Fetch Stage/FetchStage.vhd
+++ Fetch Stage/FetchStage.vhd
@@ -13,8 +13,11 @@ ENTITY FetchStage IS
 			clk: in STD_LOGIC;
 			reset: in std_logic:='0';
 			interrupt: in std_logic:='0';
-         --   IFIDBuffer: out STD_LOGIC_VECTOR(IFIDLength-1 DOWNTO 0);
-			instruction: out STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0)
+ 
+			instruction: out STD_LOGIC_VECTOR(wordSize-1 DOWNTO 0);
+			PCnew : out std_logic_vector(wordSize-1 downto 0);
+			intSignal : out std_logic; --interrupt signal output 
+			rstSignal : out std_logic --restet signal output 
 		);
 
 END FetchStage;
@@ -23,14 +26,17 @@ END FetchStage;
 ARCHITECTURE FetchStageArch of FetchStage is
 
 SIGNAL tmp:std_logic_vector(15 downto 0); --not used just dummpy variable
-
+signal dummy: std_logic;
 
 BEGIN
 
    instruction_memory: entity work.ram  port map (clk,'0','1',PCReg,tmp,instruction);
-   
-  
---   IFIDBuffer<=instruction;
+  --PCnew is a value of pc after incremented 
+   PCAdder:entity work.adder port map(PCreg,"0000000000000001",'0',PCnew,dummy);
+
+	intSignal <=interrupt;
+	--reset signal output 
+	rstSignal <=reset;
 
 
 END ARCHITECTURE;
