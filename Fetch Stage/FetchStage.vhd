504ecca2e9ec58671355eb318eafa468ae5ac700
nada-adel-mohamady <39887982+nada-adel-mohamady@users.noreply.github.com>
2020-05-06 01:05:43 +0200

    system

--- Fetch Stage/FetchStage.vhd
+++ Fetch Stage/FetchStage.vhd
@@ -8,8 +8,7 @@ ENTITY FetchStage IS
 			addressBits: integer:=32);
 			
 	PORT(
-            PCReg: in STD_LOGIC_VECTOR(addressBits-1 DOWNTO 0);
-			clk: in STD_LOGIC;
+            clk: in STD_LOGIC;
 			reset: in std_logic:='0';
 			interrupt: in std_logic:='0';
  
@@ -26,7 +25,7 @@ ARCHITECTURE FetchStageArch of FetchStage is
 
 SIGNAL tmp:std_logic_vector(15 downto 0); --not used just dummpy variable
 signal dummy: std_logic;
-
+signal PCReg: STD_LOGIC_VECTOR(PCSize-1 DOWNTO 0):="00000000000000000000000000001010";
 BEGIN
 
    instruction_memory: entity work.ram generic map(1) port map (clk,'0','1',PCReg,tmp,instruction);
@@ -36,6 +35,11 @@ BEGIN
 	intSignal <=interrupt;
 	--reset signal output 
 	rstSignal <=reset;
+	process(CLK)
+	begin
+	if(falling_edge(CLK))then 
+		PCReg <=Pcnew;
+	end process;
 
 
 END ARCHITECTURE;
