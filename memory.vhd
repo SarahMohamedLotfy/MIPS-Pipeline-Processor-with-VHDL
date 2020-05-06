04e8b179d0d2a8ece37f4463adcd420495fa2c0e
nada-adel-mohamady <39887982+nada-adel-mohamady@users.noreply.github.com>
2020-05-06 19:35:21 +0200

    fi

--- memory.vhd
+++ memory.vhd
@@ -5,33 +5,23 @@ use ieee.numeric_std.all;
 entity memory is 
 port(	
   reset, clk: in std_logic;
-<<<<<<< HEAD
-	EX_MEM:in std_logic_vector(105 downto 0);
 
-  Rsrc2,ALUresult, MemoryReuslt :out std_logic_vector(31 downto 0);
-  SWAP :out std_logic;
-  Rs,Rd,WBsignals :out std_logic_vector(3-1 downto 0)
-=======
 	EX_MEM:in std_logic_vector(114 downto 0);
 
   Rsrc2,ALUresult, MemoryReuslt :out std_logic_vector(31 downto 0);
   SWAP :out std_logic;
   Rs,Rd,WBsignals :out std_logic_vector(2 downto 0)
->>>>>>> 8196e8d787ba1f88a9b1660a68eb819caf1de23b
+
 );
 end entity;
 
 architecture memory_arch of memory is
  
-<<<<<<< HEAD
-  signal Rsrc2:std_logic_vector(31 downto 0);
-	signal SWAP, interrupt,RRI,CRR:  std_logic;
-	signal MEMsignals : std_logic_vector(3-1 downto 0);
-=======
+
 	signal interrupt,RRI:  std_logic;
 	signal MEMsignals: std_logic_vector(3 downto 0);
 	signal CRR : std_logic_vector(2 downto 0);
->>>>>>> 8196e8d787ba1f88a9b1660a68eb819caf1de23b
+
 	signal spType :std_logic_vector(2-1 downto 0);
 	signal Address : std_logic_vector(31 downto 0);
 	
@@ -48,11 +38,9 @@ interrupt<= EX_MEM(103);
 RRI<= EX_MEM(104);
 CRR<= EX_MEM(107 downto 105);
 MEMsignals<=EX_MEM(111 downto 108);
-<<<<<<< HEAD
-WBsignals<=(114 downto 112);
-=======
+
 WBsignals<=EX_MEM(114 downto 112);
->>>>>>> 8196e8d787ba1f88a9b1660a68eb819caf1de23b
+
 MemoryReuslt<=(others=>'0');
 
 end architecture;
